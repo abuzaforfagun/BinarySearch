a261ade74651dc2945a4fe2eb957aa32ee4618f1
Abu Zafor Fagun <mdabuzaforfagun@gmail.com>
2017-12-02 03:20:33 +0600

    Implement binary search

---
+++ BinarySearch/BinarySearch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    public class BinarySearch
+    {
+        public static int DoSearch(int[] array, int number, int min, int max)
+        {
+            int mid = max / 2;
+            if (array[mid] == number)
+                return mid;
+            if(array[mid] > number)
+            {
+                max = mid +1;
+            }
+            else
+            {
+                min = mid-1;
+            }
+            return DoSearch(array, number, min, max);
+
+        }
+    }
+}
---
+++ BinarySearch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 10, 13, 22, 49, 100, 283, 289, 300 };
+            Console.WriteLine(BinarySearch.DoSearch(array, 22, 0, array.Length-1));
+            Console.Read();
+        }
+    }
+}
